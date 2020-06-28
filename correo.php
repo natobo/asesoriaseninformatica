87e56934837dcf09834e0999fa69cfa240f2957a
Nicolás Tobo <na.tobo@uniandes.edu.co>
2020-06-28 14:27:39 -0500

    ensayando correo php

--- correo.php
+++ correo.php
@@ -2,6 +2,12 @@
   $nombre = $_POST['nombre'];
   $correo = $_POST['correo'];
   $numTelefono = $_POST['numTelefono'];
+   
+  if(mail('infoasesoriaseninformatica@gmail.com','Posible cliente',$nombre." ".$correo." ".$numTelefono)){
+     echo "Solicitud enviada"
+  }
+  else{
+    echo "Solicitud no enviada"
+  }
 
-  echo $nombre;
->
+?>
