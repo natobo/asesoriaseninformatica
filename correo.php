4ef49a8da3929fbfe2d0bd37a3d4b1bcc76a5a07
Nicolás Andrés Tobo Urrutia <na.tobo@uniandes.edu.co>
2020-06-29 21:09:10 -0500

    Cambios freddy
    bla

--- correo.php
+++ correo.php
@@ -2,12 +2,12 @@
   $nombre = $_POST['nombre'];
   $correo = $_POST['correo'];
   $numTelefono = $_POST['numTelefono'];
-   
-  if(mail('infoasesoriaseninformatica@gmail.com','Posible cliente',$nombre." ".$correo." ".$numTelefono)){
-     echo "Solicitud enviada"
+
+  if(mail('informacion@asesoriaseninformatica.com','Posible cliente',$nombre)){
+     echo "Solicitud enviada";
   }
   else{
-    echo "Solicitud no enviada"
+    echo "Solicitud no enviada";
   }
 
 ?>
---
+++ correos.php
@@ -0,0 +1,7 @@
+<?php
+  $nombre = $_POST['nombre'];
+  $correo = $_POST['correo'];
+  $numTelefono = $_POST['numTelefono'];
+
+  echo $nombre;
+?>
